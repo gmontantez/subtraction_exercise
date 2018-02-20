567afede7df4e390776333ce047345e2a6e76354
gmontantez <gmontantez7@gmail.com>
2018-02-20 14:45:10 -0500

    Test Exercise for Subtraction

---
+++ test_subtraction_exercise.rb
@@ -0,0 +1,19 @@
+require "minitest/autorun"
+require_relative "subtraction_exercise.rb"
+
+class Subtraction_test < Minitest::Test
+	def test_1_equals_1
+		#assert_equal(1,1)
+	end
+	def test_1_not_equals_1
+		#assert_equal(1,2)
+	end
+	def test_2_equals_2
+		#assert_equal(2,subtraction(1,1))
+	end
+	def test_8_minus_3_equals_5
+		#assert_equal(5,subtraction(8,3))
+	end
+
+end
+	
